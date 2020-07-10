389bbd2e75e2e46f73a884fb259cbd9a93975a7c
Samir Hassan Ayoub <samir.ayoub@SPOMM010114176.local>
2020-07-10 15:04:33 -0300

    refactor(all): remove a opção de executar a função no contexto pai
    Anteriormente era possível passar funções para as propriedades sem informar o .bind(this). Agora é necessário passar a referência através do .bind(this) para que o mesmo execute a função no contexto invocado, tanto em funções dentro de arrays quanto em funções via property bind.
    
    Fixes DTHFUI-3692
    
    BREAKING CHANGES: remove a opção de executar a função no contexto pai
    
    Anteriormente era possível passar funções para as propriedades sem informar o .bind(this). Agora é necessário passar a referência através do .bind(this) para que o mesmo execute a função no contexto invocado, tanto em funções dentro de arrays quanto em funções via property bind.
    
    Componentes Afetados:
    ButtonGroup
    Menu
    MenuPanel
    Navbar
    PageDefault
    PageList
    PageChangePassword
    Popup
    Table
    Toolbar

--- projects/ui/src/lib/components/po-page/po-page-list/po-page-list.component.ts
+++ projects/ui/src/lib/components/po-page/po-page-list/po-page-list.component.ts
@@ -100,7 +100,7 @@ export class PoPageListComponent extends PoPageListBaseComponent
     if (item.url) {
       isExternalLink(item.url) ? openExternalLink(item.url) : this.router.navigate([item.url]);
     } else if (item.action) {
-      callFunction(item.action, this.parentRef);
+      item.action();
     }
   }
 
@@ -151,8 +151,8 @@ export class PoPageListComponent extends PoPageListBaseComponent
     }
   }
 
-  callActionFilter(field: string): void {
-    this.callFunction(this.filter[field], this.parentRef, this.filterInput.nativeElement.value);
+  callActionFilter(field: string) {
+    this.filter[field]();
     this.changeDetector.detectChanges();
   }
 
